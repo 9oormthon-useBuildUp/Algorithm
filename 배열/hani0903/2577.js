c44b97fdb9173f24f09fe703cd5694144637d8d0
조하은 <jhe93@naver.com>
2024-08-27 20:42:05 +0900

    [백준 2577]: 숫자의 개수
    2577번 문제 풀이 완료하였습니다.

---
+++ 배열/hani0903/2577.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? 'dev/stdin' : '../../input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').split('\n').map(Number);;
+
+const freq = Array.from({length:10}).fill(0);
+
+let result =  input[0] * input[1] * input[2];
+
+result = result.toString();
+
+for(const c of result){
+    freq[Number(c)]++;
+}
+
+console.log(freq.join('\n'));
+
