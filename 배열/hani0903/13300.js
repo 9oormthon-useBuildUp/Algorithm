7296d60f3b48f12c1bff7f4443fd7470823303d4
조하은 <jhe93@naver.com>
2024-08-27 20:43:05 +0900

    [백준 13300]: 방 배정
    13300번 문제 풀이 완료하였습니다.

---
+++ 배열/hani0903/13300.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? 'dev/stdin' : '../../input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').split('\n');
+
+let roomCnt = 0;
+
+let [N, k] = input[0].split(' ').map(Number);
+
+let girls = Array.from({length: 6}).fill(0);
+let boys = Array.from({length: 6}).fill(0);
+
+for(let i = 1; i <= N; i++){
+    let [S, Y] = input[i].split(' ').map(Number);
+
+    if(S == 0){
+        girls[Y-1]++;
+    }else{
+        boys[Y-1]++;
+    }
+}
+
+for (let i = 0; i < 6; i++ ){
+    roomCnt += Math.ceil(girls[i]/ k)
+    roomCnt += Math.ceil(boys[i]/ k)
+}
+
+console.log(roomCnt);
