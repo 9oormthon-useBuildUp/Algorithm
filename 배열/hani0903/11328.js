0f3aee31f7a66eb8730ef6cbcce1249252cf77ca
조하은 <jhe93@naver.com>
2024-08-28 13:31:52 +0900

    [백준 11328]: Strfry

---
+++ 배열/hani0903/11328.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : '../../input'
+const input =  fs.readFileSync(filePath, 'utf-8').split('\n');
+
+for(let i = 1; i <= input[0]; i ++){
+    
+    let [word1, word2] = input[i].split(' ');
+
+    //정렬 후 같은지 비교
+    if(word1.split('').sort().join('') === word2.split('').sort().join('')){
+        console.log("Possible");
+    }else{
+        console.log("Impossible");
+    }
+}
