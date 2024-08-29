80c80d3708e9de30ab9c1afa43dec899c5194774
조하은 <jhe93@naver.com>
2024-08-30 01:06:43 +0900

    [백준 3273]: 두 수의 합
    투포인터를 사용하여 문제 풀이 완료하였습니다.

---
+++ 배열/hani0903/3273.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const filePath = process.platform === 'linux' ? 'dev/stdin' : '../../input.txt';
+const input = fs.readFileSync(filePath, 'utf-8').split('\n');
+
+let numbers = input[1].split(' ').map(Number);
+let x = Number(input[2]);
+
+//배열 오름차순 정렬
+numbers = numbers.sort((a, b)=> a - b);
+
+let count = 0;
+let start = 0;
+let end = numbers.length - 1;
+
+//투 포인터 사용
+while(start < end){
+
+    let sum = numbers[start] + numbers[end];
+    
+    if(sum === x ){
+        count++;
+        start++;
+        end--;
+        
+    }else if(sum > x){
+        end--;
+    }else{
+        start++;
+    }
+}
+
+console.log(count);
