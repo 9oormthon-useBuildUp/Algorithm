69fb3711142ffbd784aebaeb4746aba6b75b5935
AhnRian <00rian@naver.com>
2024-09-03 15:15:51 +0900

    커밋 수정

--- 비타알고/rian00/인싸가되고싶은민수2.py
+++ 비타알고/rian00/인싸가되고싶은민수2.py
@@ -2,7 +2,6 @@ def findMinDivisor(n):
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return i
-    # 만약 약수가 발견되지 않으면, b는 소수
     return n
 
 a,b=map(int,input().split(' '))
